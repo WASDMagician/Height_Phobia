2a54e85471cc92c6757457a087d15be786cd1aa5
Joseph Adams <WASDMagician@gmail.com>
2017-12-01 13:09:26 +0000

    Folder Setup

---
+++ Height_Phobia/Assets/Editor/Auto_Save_On_Run.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class Auto_Save_On_Run
+{
+    static Auto_Save_On_Run()
+    {
+        EditorApplication.playmodeStateChanged = () =>
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                Debug.Log("Auto Save Run");
+                EditorSceneManager.SaveOpenScenes();
+                AssetDatabase.SaveAssets();
+            }
+        };
+    }
+}
