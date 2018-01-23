f02f207b32b9519d0479dda6e06c4002923f233b
Joseph Adams <WASDMagician@gmail.com>
2018-01-23 11:32:54 +0000

    whiteboxing

---
+++ Height_Phobia/Assets/PROTO_Grey_Randomiser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PROTO_Grey_Randomiser : MonoBehaviour {
+
+    public GameObject[] items_to_grey;
+
+	// Use this for initialization
+	void Start () {
+	    for(int i = 0; i < items_to_grey.Length; i++)
+        {
+            if(items_to_grey[i] != null)
+            {
+                MeshRenderer renderer = items_to_grey[i].GetComponent<MeshRenderer>();
+                if(renderer != null)
+                {
+                    Material[] materials = renderer.materials;
+                    for(int j = 0; j < materials.Length; j++)
+                    {
+                        float grey = Random.Range(0.0f, 1f);
+                        Color color = new Color(grey, grey, grey);
+                        materials[j].color = color;
+                    }
+                }
+            }
+        }
+	}
+	
+}
---
+++ Height_Phobia/Assets/PROTO_Player_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PROTO_Player_Controller : MonoBehaviour {
+
+    public GameObject head_object;
+    public float move_speed, turn_speed;
+    Rigidbody player_rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        player_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Handle_User_Input();
+	}
+
+    void Handle_User_Input()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        float grav = player_rigidbody.velocity.y;
+        Vector3 movement = (this.transform.forward * move_speed * ver) + (this.transform.right * move_speed * hor);
+        movement.y = grav;
+        player_rigidbody.velocity = movement;
+
+        float m_hor = Input.GetAxis("Mouse X");
+        float m_ver = Input.GetAxis("Mouse Y");
+        this.transform.Rotate(new Vector3(0, turn_speed * m_hor * Time.deltaTime, 0));
+        head_object.transform.Rotate(new Vector3(-turn_speed * m_ver * Time.deltaTime, 0, 0));
+    }
+    
+}
