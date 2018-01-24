b257f2b0e54b912c8abdd7b241ec3be45e4d2880
Joseph Adams <WASDMagician@gmail.com>
2018-01-24 11:26:17 +0000

    shit

---
+++ Height_Phobia/Assets/Resonai/Editor/ResonaiMeshDetailsInspectorProxy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Ywz
+{
+  [CustomEditor(typeof(ResonaiMeshDetails))]
+  public class ResonaiMeshDetailsInspectorProxy : Editor
+  {
+    ResonaiMeshDetailsInspector inspector;
+
+    public ResonaiMeshDetailsInspectorProxy()
+    {
+      inspector = new ResonaiMeshDetailsInspector(this);
+    }
+
+    public void OnEnable()
+    {
+      inspector.SetTarget(target);
+    }
+
+    public override void OnInspectorGUI()
+    {
+      inspector.OnInspectorGUI();
+    }
+  }
+}
