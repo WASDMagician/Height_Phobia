7eabcc6c9d89291972fd7416d65c162ec7b52f28
Conor <crobertson696@gmail.com>
2018-01-25 11:18:02 +0000

    Revert "Revert "Merge branch 'master' of https://github.com/WASDMagician/Height_Phobia""
    This reverts commit 7f8cf93dfafb88d7bbc452a258cb1ce653b4d55b.

---
+++ Height_Phobia/Assets/Tool_Remove_Collider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Tool_Remove_Collider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            DestroyImmediate(colliders[i]);
+        }
+        DestroyImmediate(this);
+	}
+}
