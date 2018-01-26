4ada262158b77bdea8ad0287fd7e9953de8e097b
Darius0995 <dariussparkes669@gmail.com>
2018-01-26 18:00:49 +0000

    Revert "Merge branch 'master' of https://github.com/WASDMagician/Height_Phobia"
    This reverts commit b29eb2fdf202ae40ce6e4ff83529726e10f94d87, reversing
    changes made to 36a8524a5b64dba2e73d97065d4318b229e0643e.

---
+++ Height_Phobia/Assets/Resonai/Scripts/ResonaiMeshDetails.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Ywz
+{
+  #if UNITY_EDITOR
+  public class ResonaiMeshDetails : ResonaiMeshDetailsInternal
+  {
+  }
+  #else
+  public class ResonaiMeshDetails : MonoBehaviour
+  {
+  }
+  #endif
+}
